61e55b6c491b150bc2b3fbf1f3377bfbd8aefdb1
VissaMoutafis <sdi1800119@di.uoa.gr>
2021-10-09 17:14:20 +0300

    final + README <3

--- Project/challenge/my_PBS_script_16176.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#PBS -N J_seq_1_840
-#PBS -q N10C80
-#PBS -l walltime=00:20:00
-#PBS -l select=1:ncpus=1:mem=16400000kb
-cd $PBS_O_WORKDIR
-mpirun -np 1 jacobi_serial_opt.x < input
--- Project/seqJacobi/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mpicc jacobi_serial_opt.c -o jacobi_serial_opt.x -lm -O3
-qsub PBSseqJ.sh
